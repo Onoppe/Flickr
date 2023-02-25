cca7b9afdbb6b2d1ed9375aa6d309f40d2be5e12
Omar Noppe <omar.noppe@getir.com>
2023-02-25 20:08:09 +0100

    Implement fetch logic in `ListViewModel`
    Signed-off-by: Omar Noppe <omar.noppe@getir.com>

--- FlickrFinder/FlickrFinder/Scenes/List/ViewModels/ListViewModel.swift
+++ FlickrFinder/FlickrFinder/Scenes/List/ViewModels/ListViewModel.swift
@@ -13,9 +13,46 @@ final class ListViewModel: ObservableObject {
 
     private let service: FlickrSearchServicable
 
+    // MARK: Internal
+
+    @Published var photos: PhotoFeed.Photos = []
+    @Published var searchText = ""
+
     // MARK: Initialiser
 
     init(service: FlickrSearchServicable) {
         self.service = service
+    }
+}
+
+// MARK: - Internal Methods
+
+extension ListViewModel {
+
+    func getItems() {
+        Task(priority: .background) {
+            await fetchItems()
+        }
+    }
+}
+
+// MARK: - Private Helper Methods
+
+
+extension ListViewModel {
+
+    private func fetchItems() async {
+        let result = await service.search(with: FlickrEndpoint.search(text: searchText, page: 0))
+
+        switch result {
+        case .success(let response):
+            await MainActor.run { [weak self] in
+                guard let self else { return }
+
+                self.photos = response.feed.photos
+            }
+        default:
+            break
+        }
     }
 }
