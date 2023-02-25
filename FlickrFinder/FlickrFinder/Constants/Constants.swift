687a06878e7584b11b425c52a5ef4574be1b99b8
Omar Noppe <omar.noppe@getir.com>
2023-02-25 19:35:47 +0100

    Add Constants enum in the project
    Signed-off-by: Omar Noppe <omar.noppe@getir.com>

---
+++ FlickrFinder/FlickrFinder/Constants/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  FlickrFinder
+//
+//  Created by Omar Noppe on 25/02/2023.
+//
+
+import Foundation
+
+enum Constants {
+    static let apiKey = "1508443e49213ff84d566777dc211f2a"
+}
