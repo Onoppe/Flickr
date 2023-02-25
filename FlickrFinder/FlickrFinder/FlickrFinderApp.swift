224c5fa8e745951d5b8da32f785a643376bdf652
Omar Noppe <omar.noppe@getir.com>
2023-02-25 16:03:09 +0100

    Add Xcode project
    Signed-off-by: Omar Noppe <omar.noppe@getir.com>

---
+++ FlickrFinder/FlickrFinder/FlickrFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrFinderApp.swift
+//  FlickrFinder
+//
+//  Created by Omar Noppe on 25/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlickrFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
