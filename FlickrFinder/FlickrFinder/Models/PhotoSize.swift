e4e4d0bcd3e9e1de745b730332fdc4edf34c4c42
Omar Noppe <omar.noppe@getir.com>
2023-02-25 16:34:59 +0100

    Add `PhotoSize` enum to differentiate sizes
    Signed-off-by: Omar Noppe <omar.noppe@getir.com>

---
+++ FlickrFinder/FlickrFinder/Models/PhotoSize.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoSize.swift
+//  FlickrFinder
+//
+//  Created by Omar Noppe on 25/02/2023.
+//
+
+import Foundation
+
+enum PhotoSize: String {
+    case normal = "b"
+    case thumbnail = "m"
+}
