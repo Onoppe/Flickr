621f90f93607db0f3a4933481b46612c4b0f77d5
Omar Noppe <omar.noppe@getir.com>
2023-02-25 16:44:21 +0100

    Add a `Endpoint` protocol
    For adding multiple different endpoints if we want to
    
    Signed-off-by: Omar Noppe <omar.noppe@getir.com>

---
+++ FlickrFinder/FlickrFinder/Services/Network/Endpoint/Endpoint.swift
@@ -0,0 +1,17 @@
+//
+//  Endpoint.swift
+//  FlickrFinder
+//
+//  Created by Omar Noppe on 25/02/2023.
+//
+
+import Foundation
+
+protocol Endpoint {
+    var path: String? { get }
+    var scheme: String { get }
+    var host: String { get }
+    var method: String { get }
+    var header: [String: String]? { get }
+    var queryItems: [URLQueryItem] { get }
+}
