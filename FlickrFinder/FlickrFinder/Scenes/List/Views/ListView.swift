113ddd6f404bbfeaf25aa7dd20a5a39d5c5ed5ea
Omar Noppe <omar.noppe@getir.com>
2023-02-25 21:13:36 +0100

    Add a placeholder for images in the `ListView`
    Signed-off-by: Omar Noppe <omar.noppe@getir.com>

--- FlickrFinder/FlickrFinder/Scenes/List/Views/ListView.swift
+++ FlickrFinder/FlickrFinder/Scenes/List/Views/ListView.swift
@@ -10,14 +10,19 @@ import SwiftUI
 struct ListView: View {
 
     @ObservedObject var viewModel = ListViewModel(service: FlickrSearchService())
-
+    
     var body: some View {
         NavigationView {
             List(viewModel.photos) { photo in
-
+                
                 HStack {
-                    AsyncImage(url: photo.getUrl(using: .thumbnail))
-                        .frame(width: 40, height: 40)
+                    AsyncImage(url: photo.getUrl(using: .thumbnail)) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                    } placeholder: {
+                        Image(systemName: "photo.fill")
+                    }.frame(width: 50, height: 50)
                         .cornerRadius(10)
                     Text(photo.title ?? "")
                 }
